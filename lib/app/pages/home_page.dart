7f854234e69f2240fe866b1ce80380232dbbdbb4
Nika0009 <nikainasaridze99@icloud.com>
2024-03-01 22:09:55 +0400

    modified:   lib/app/pages/home_page.dart 	deleted:    lib/data/blocs/upload/upload_cubit.dart 	deleted:    lib/data/blocs/upload/upload_state.dart 	deleted:    lib/data/models/tus_file.dart 	deleted:    lib/data/services/storage_service.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -1,14 +1,10 @@
-import 'package:cross_file/cross_file.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moon_design/moon_design.dart';
 import 'package:superfrog/app/pages/splash_page.dart';
 import 'package:superfrog/data/blocs/authentication/authentication_bloc.dart';
 import 'package:superfrog/data/blocs/common_bloc.dart';
-import 'package:superfrog/data/blocs/upload/upload_cubit.dart';
 import 'package:superfrog/utils/extensions.dart';
-import 'package:uuid/uuid.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,7 +14,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final Uuid uuid = Uuid();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -115,75 +110,6 @@ class _HomePageState extends State<HomePage> {
                           label: const Text('Sign Out'),
                         ),
                         const SizedBox(height: 24.0),
-                        BlocProvider(
-                          create: (_) => UploadCubit(),
-                          child: BlocBuilder<UploadCubit, UploadState>(
-                            builder: (context, state) {
-                              state.whenOrNull(loaded: (val) => print(val.length));
-                              return Column(
-                                children: [
-                                  MoonFilledButton(
-                                    onTap: () async {
-                                      await FilePicker.platform.pickFiles().then((result) {
-                                        if (result != null && result.files.isNotEmpty) {
-                                          XFile file = XFile(result.files.first.path!);
-
-                                          context.read<UploadCubit>().addFile(file);
-                                        }
-                                      });
-                                    },
-                                    isFullWidth: true,
-                                    label: Text('addFile'),
-                                  ),
-                                  const SizedBox(height: 24.0),
-                                  state.maybeWhen(
-                                    loaded: (val) => ListView.separated(
-                                      shrinkWrap: true,
-                                      itemCount: val.length,
-                                      separatorBuilder: (_, __) => SizedBox(height: 16.0),
-                                      itemBuilder: (_, index) {
-                                        return ValueListenableBuilder(
-                                          valueListenable: val[index].state!,
-                                          builder: (context, value, child) {
-                                            return MoonAlert(
-                                              show: true,
-                                              showBorder: true,
-                                              borderColor: context.moonColors?.beerus,
-                                              backgroundColor: context.moonColors?.gohan,
-                                              label: value.when(
-                                                onUploadStart: () => Text('onStart'),
-                                                onUploadProgress: (val) => Text('onProgress $val'),
-                                                onUploadCompleted: () => Text('completed'),
-                                              ),
-                                              /* SelectableText(
-                                                val[index].objectId,
-                                              ) */
-                                              leading: MoonButton.icon(
-                                                buttonSize: MoonButtonSize.sm,
-                                                icon: const MoonIcon(MoonIcons.generic_upload_24_light),
-                                                onTap: () {
-                                                  context.read<UploadCubit>().upload(val[index].objectId);
-                                                },
-                                              ),
-                                              trailing: MoonButton.icon(
-                                                buttonSize: MoonButtonSize.sm,
-                                                icon: const MoonIcon(MoonIcons.controls_close_small_24_light),
-                                                onTap: () {
-                                                  context.read<UploadCubit>().removeFile(val[index].objectId);
-                                                },
-                                              ),
-                                            );
-                                          },
-                                        );
-                                      },
-                                    ),
-                                    orElse: () => Text('no items'),
-                                  ),
-                                ],
-                              );
-                            },
-                          ),
-                        )
                       ],
                     ),
                   ),
