bd6b1ebe6058775cf203d5ca528fbfb01d3d2e4e
Nika Inasaridze <nikainasaridze99@icloud.com>
2024-07-05 00:25:07 +0400

    fix:    Android CI build failure     feat:   Removed unnecessary packages

--- lib/utils/platform.dart
+++ lib/utils/platform.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 
 class PlatformUtils {
   static bool get isMobile {
@@ -18,11 +17,5 @@ class PlatformUtils {
     } else {
       return Platform.isLinux || Platform.isFuchsia || Platform.isWindows || Platform.isMacOS;
     }
-  }
-
-  static Future<String> get appScheme async {
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
-
-    return kIsWeb ? 'http://' : '${packageInfo.packageName}://';
   }
 }
